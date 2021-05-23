9502ae3b3770126bc3170234fed055045d0f501d
TonyMoon <tonycontinue@gmail.com>
2021-05-22 23:12:57 -0700

    fixed language bug and uppercase error of file type

--- lambda/index.js
+++ lambda/index.js
@@ -6,10 +6,10 @@ const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
     const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
-    const Key = event.Records[0].s3.object.key; // original/123123123_abc.png
+    const Key = decodeURICompenent(event.Records[0].s3.object.key); // original/123123123_abc.png
     console.log(Bucket, Key);
     const filename = Key.split('/')[Key.split('/').length-1];
-    const ext = Key.split('.')[Key.split('.').length-1];
+    const ext = Key.split('.')[Key.split('.').length-1].toLowerCase();
     const requiredFormat = ext === 'jpg' ? 'hpeg' : ext;
     console.log('filename', filename, 'ext', ext);
 
