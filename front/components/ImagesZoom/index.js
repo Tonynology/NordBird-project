f579d5afe4dd85200ef9346b902b6e0b884e45d5
TonyMoon <tonycontinue@gmail.com>
2021-05-30 22:14:05 -0700

    change image resizing when it clicked

--- front/components/ImagesZoom/index.js
+++ front/components/ImagesZoom/index.js
@@ -4,7 +4,7 @@ import Slick from 'react-slick';
 
 
 import { Overlay, Header, CloseBtn, SlickWrapper, ImgWrapper, Indicator, Global } from './styles';
-import { backUrl } from '../../config/config';
+// import { backUrl } from '../../config/config';
 
 const ImagesZoom = ({ images, onClose }) => {
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -27,7 +27,7 @@ const ImagesZoom = ({ images, onClose }) => {
                 >
                     {images.map((v) => (
                         <ImgWrapper key={v.src}>
-                            <img src={`${backUrl}/${v.src}`} alt={v.src} />
+                            <img src={`${v.src.replace('/thumb/', /\/original\//)}`} alt={v.src} />
                         </ImgWrapper>
                     ))}
                 </Slick>
