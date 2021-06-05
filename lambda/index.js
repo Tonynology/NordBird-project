b3058e787597083e77a975af441635fe6ba13612
TonyMoon <tonycontinue@gmail.com>
2021-06-04 17:15:47 -0700

    fixed bug in lambda

--- lambda/index.js
+++ lambda/index.js
@@ -5,8 +5,8 @@ const s3 = new AWS.S3();
 
 
 exports.handler = async (event, context, callback) => {
-    const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
-    const Key = decodeURICompenent(event.Records[0].s3.object.key); // original/123123123_abc.png
+    const Bucket = event.Records[0].s3.bucket.name; // react-blueboard-s3
+    const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/123123123_abc.png
     console.log(Bucket, Key);
     const filename = Key.split('/')[Key.split('/').length-1];
     const ext = Key.split('.')[Key.split('.').length-1].toLowerCase();
